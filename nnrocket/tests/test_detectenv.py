2b1b8513913febf4f6f25e741c831f5907038959
Tim Dinger <67650729+trdinger@users.noreply.github.com>
2022-03-15 14:31:28 -0400

    Create test_detectenv.py

---
+++ nnrocket/tests/test_detectenv.py
@@ -0,0 +1,13 @@
+# IBM Time Series Classification version 1.0.0
+# IBM Confidential
+# Copyright IBM Corp., 2021
+
+import os 
+import sys
+
+def test_singlenode():
+    assert True
+
+
+def test_multinodes():
+    assert True
